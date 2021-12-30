23d1d6b3ce03c4b8b264eb7be7b51b5bd2ed2988
Michal Andronik <michal.androni@gmail.com>
2021-12-30 02:22:48 +0100

    test commit

--- src/components/features/DaysToSummer/DaysToSummer.js
+++ src/components/features/DaysToSummer/DaysToSummer.js
@@ -5,8 +5,18 @@ import { formatTime } from '../../../utils/formatTime';
 
 class DaysToSummer extends React.Component {
 
+  // getCountdownTime(){
+  //   const currentTime = new Date();
+  //   const nextNoon = new Date(Date.UTC(currentTime.getUTCFullYear(), currentTime.getUTCMonth(), currentTime.getUTCDate(), 12, 0, 0, 0));
+  
+  //   if(currentTime.getUTCHours() >= 12){
+  //     nextNoon.setUTCDate(currentTime.getUTCDate()+1);
+  //   }
+  
+  //   return Math.round((nextNoon.getTime() - currentTime.getTime())/1000);
+  // }
   getCountdownTime(){
-    const currentTime = new Date();
+    const currentTime = new Date("Jan 5, 2022 15:37:25");
     const nextNoon = new Date(Date.UTC(currentTime.getUTCFullYear(), currentTime.getUTCMonth(), currentTime.getUTCDate(), 12, 0, 0, 0));
   
     if(currentTime.getUTCHours() >= 12){
@@ -18,11 +28,11 @@ class DaysToSummer extends React.Component {
 
   render() {
     let getCountdownTime = this.getCountdownTime();
-    let { promoDescription, title } = this.props;
+    let { promoDescription, title, summer} = this.props;
     return (
       <div className={styles.component} >
         <div className={styles.title}>{title}</div>
-        <div className={styles.days}></div>
+        <div className={styles.days}>{getCountdownTime}</div>
       </div>
     );
   };
@@ -33,3 +43,30 @@ DaysToSummer.propTypes = {
 };
 
 export default DaysToSummer;
+
+// var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+
+// var x = setInterval(function() {
+
+//   // Get today's date and time
+//   var now = new Date().getTime();
+
+//   // Find the distance between now and the count down date
+//   var distance = countDownDate - now;
+
+//   // Time calculations for days, hours, minutes and seconds
+//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+//   // Display the result in the element with id="demo"
+//   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+//   + minutes + "m " + seconds + "s ";
+
+//   // If the count down is finished, write some text
+//   if (distance < 0) {
+//     clearInterval(x);
+//     document.getElementById("demo").innerHTML = "EXPIRED";
+//   }
+// }, 1000);
